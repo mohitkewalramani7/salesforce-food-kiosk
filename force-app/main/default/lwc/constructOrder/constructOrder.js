aaef237692db03847db58b2b04a2b29038172438
Mohit Kewalramani <mohitkewalramani74@gmail.com>
2023-06-12 22:48:26 -0700

    Constructed LWC to build an order with food items

---
+++ force-app/main/default/lwc/constructOrder/constructOrder.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class ConstructOrder extends LightningElement {
+    orderItems = [{'id': 1, 'foodItem': '', 'quantity': 1, 'requests': ''}]
+
+}
