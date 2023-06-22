ac25643142dbbade4373502bf18127a0499870bb
Mohit Kewalramani <mohitkewalramani74@gmail.com>
2023-06-21 18:47:28 -0700

    Continued working on order LWC component

--- force-app/main/default/lwc/constructOrder/constructOrder.js
+++ force-app/main/default/lwc/constructOrder/constructOrder.js
@@ -2,5 +2,10 @@ import { LightningElement } from 'lwc';
 
 export default class ConstructOrder extends LightningElement {
     orderItems = [{'id': 1, 'foodItem': '', 'quantity': 1, 'requests': ''}]
+    menuItems = [
+        { label: 'New', value: 'new' },
+        { label: 'In Progress', value: 'inProgress' },
+        { label: 'Finished', value: 'finished' },
+    ];
 
 }
